2d25c969f6c8746b388b9a1ce52f91e05000b54e
Ubity <eltonbor@gmail.com>
2016-11-23 13:57:36 -0500

    new version: i18n + allow dev domain

---
+++ chrome/create-zip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+rm -r chrome.zip
+
+
+/usr/bin/zip chrome.zip \
+ background.js \
+ _locales/*/* \
+ manifest.json \
+ ubity-logo-128x128.png \
+ ubity-logo-16x16.png \
+ ubity-logo-48x48.png \
+ 
